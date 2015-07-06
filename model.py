9c90d801c9c09d8cb08bf6c61f588dee4f7dc313
Rodrigo Stevaux <rodrigo@stevaux.com>
2015-07-06 20:14:41 -0300

    Improved model with RFs

--- model.py
+++ model.py
@@ -1,38 +1,69 @@
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
+
+train_set = pd.read_csv('raw/train_set.csv', parse_dates=[2])
+train_set = pd.concat([pd.get_dummies(train_set.bracket_pricing), train_set], axis=1)
+
+train_set['year'] = train_set.quote_date.dt.year
+train_set['month'] = train_set.quote_date.dt.month
+train_set['day'] = train_set.quote_date.dt.day
+
+# data from tube.csv w/ dummies
+tube = pd.read_csv('raw/tube.csv')
+tube_components = pd.read_csv('processed/tube_components.csv')
+dummies_material = pd.get_dummies(tube['material_id'])
+dummies_end_a = pd.get_dummies(tube['end_a'])
+dummies_end_x = pd.get_dummies(tube['end_x'])
+tube_specs = pd.read_csv('processed/tube_specs.csv')
+
+df_train = pd.concat([dummies_end_x, dummies_end_a, dummies_material, tube], axis=1)
+df_train = pd.merge(df_train, tube_components, on='tube_assembly_id', how='left')
+df_train = pd.merge(df_train, tube_specs, on='tube_assembly_id', how='left')
+
+df_train = pd.merge(train_set, df_train, on='tube_assembly_id', how='left')
+
+numeric = [np.float64, np.int64]
+
+train_x = df_train.drop('cost', axis=1).select_dtypes(numeric).fillna(0)
+train_y = df_train.cost
+
+RF = RandomForestRegressor(random_state=0, n_estimators=100)
+RF.fit(train_x, np.log1p(train_y))
+
+print RF.score(train_x, np.log1p(train_y))
+
+# PREDIT TEST SET
+
+test_set = pd.read_csv('raw/test_set.csv', parse_dates=[3])
+test_set = pd.concat([pd.get_dummies(test_set.bracket_pricing), test_set], axis=1)
+
+test_set['year'] = test_set.quote_date.dt.year
+test_set['month'] = test_set.quote_date.dt.month
+test_set['day'] = test_set.quote_date.dt.day
+
+# data from tube.csv w/ dummies
+df_test = pd.concat([dummies_end_x, dummies_end_a, dummies_material, tube], axis=1)
+df_test = pd.merge(df_test, tube_components, on='tube_assembly_id', how='left')
+df_test = pd.merge(df_test, tube_specs, on='tube_assembly_id', how='left')
+
+df_test = pd.merge(test_set, df_test, on='tube_assembly_id', how='left')
+
+print df_train.shape
+print df_test.shape
+
+numeric = [np.float64, np.int64]
+
+test_x = df_test.select_dtypes(numeric).fillna(0).drop('id', axis=1)
+
+predictions = [(i+1, p) for i, p in enumerate(np.expm1(RF.predict(test_x)))]
+
+f = open('submission.csv','w')
+f.write("id,cost\n")
+
+for i, p in predictions:
+    f.write(str(i) + "," + str(p) + "\n")
+
+f.close()
 
 
-tubes = pd.read_csv('raw/tube.csv')
-materials = pd.get_dummies(tubes['material_id'])
-end_x = pd.get_dummies(tubes['end_x'])
-end_a = pd.get_dummies(tubes['end_a'])
-
-tube_assembly_features = pd.concat([materials, tubes, end_a, end_x], axis=1)
-
-train_set = pd.read_csv('raw/train_set.csv')
-bracket_pricing = pd.get_dummies(train_set['bracket_pricing'])
-
-train_set = pd.concat([bracket_pricing, train_set], axis=1)
-
-df = pd.merge(train_set, tube_assembly_features, on='tube_assembly_id')
-
-print df.shape
-print train_set.shape
-
-assert df.shape[0] == train_set.shape[0]
-
-columns = list(materials.columns) + ['quantity', 'length', 'wall', 'diameter'] + list(end_x.columns) + list(end_a.columns)
-
-train_x = df.select_dtypes([np.float64]).fillna(0).drop('cost', axis=1)
-
-for c in train_x.columns:
-    if not c in columns:
-        train_x.drop(c, axis=1, inplace=True)
-
-train_y = np.log1p(df.cost)
-
-LR = linear_model.LinearRegression()
-LR.fit(train_x, train_y)
-
-print LR.score(train_x, train_y)
---
+++ relational_specs.py
@@ -0,0 +1,24 @@
+# Convert bill of materials to a more tractable format
+
+import pandas as pd
+
+df = pd.read_csv('raw/specs.csv')
+
+rows_output = []
+
+for i in range(len(df)):
+    row = df.loc[i]
+    ta = row['tube_assembly_id']
+    for j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+
+        v = row['spec' + str(j)]
+
+        if type(v) == str:
+            rows_output.append((ta, v))
+
+columns=['tube_assembly_id', 'component_id']
+
+df = pd.DataFrame(rows_output, columns=columns)
+df['x'] = 1
+df = pd.pivot_table(df, index='tube_assembly_id', columns='component_id', values='x')
+df.to_csv('processed/tube_specs.csv')
