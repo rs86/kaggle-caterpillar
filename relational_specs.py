d6754947a0661e010f1f22a69db6707201c9f59a
Rodrigo Stevaux <rodrigo@stevaux.com>
2015-07-07 00:01:37 -0300

    ..

--- relational_specs.py
+++ relational_specs.py
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 # Convert bill of materials to a more tractable format
 
+=======
+>>>>>>> 12055b450bc8d347ea310f2a1934816764fac940
 import pandas as pd
 
 df = pd.read_csv('raw/specs.csv')
 
+<<<<<<< HEAD
 rows_output = []
 
 for i in range(len(df)):
@@ -22,3 +26,17 @@ df = pd.DataFrame(rows_output, columns=columns)
 df['x'] = 1
 df = pd.pivot_table(df, index='tube_assembly_id', columns='component_id', values='x').fillna(0)
 df.to_csv('processed/tube_specs.csv')
+=======
+lines = []
+
+for i in df.index:
+    row = df.loc[i]
+    tube_assembly_id = row['tube_assembly_id']
+    for j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+        k = 'spec' + str(j)
+        if type(row[k]) == str:
+            lines.append((tube_assembly_id, row[k]))
+
+new_df = pd.DataFrame(lines, columns=['tube_assembly_id', 'spec_id'])
+new_df.to_csv('aux/relational_specs.csv', index=False)
+>>>>>>> 12055b450bc8d347ea310f2a1934816764fac940
